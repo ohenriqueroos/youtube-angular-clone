979d5cd1ddf04922cb8ccedeca767a8db8b70a6c
Henrique Roos <henriquemroos21@gmail.com>
2023-02-21 14:36:22 -0300

    feat: Card Interface

---
+++ src/app/components/card/Card.ts
@@ -0,0 +1,8 @@
+export interface Card {
+    image: string
+    title: string
+    nameChannel: string
+    imageChannel: string
+    views: string
+    days: string
+}
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from './Card';
 
 @Component({
   selector: 'app-card',
@@ -6,15 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  cards: {image: string, title: string, nameChannel: string, imageChannel: string, views: string, days: string}[] = [
-    {"image": "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "title": "Day in the life of a Software Engineer",
-      "nameChannel": "Nome do Canal",
-      "imageChannel": "https://images.pexels.com/photos/13471116/pexels-photo-13471116.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "views": "50 mil",
-      "days": "10 dias"
-    },
-  ]
+  cards: Card[] = [
+    { 
+      image: "https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
+      title: "Day in the life of a Software Engineer",
+      nameChannel: "Nome do Canal",
+      imageChannel: "https://images.pexels.com/photos/13471116/pexels-photo-13471116.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      views: "50 mil",
+      days: "10 dias"
+    }
+  ];
 
   constructor() { }
 
