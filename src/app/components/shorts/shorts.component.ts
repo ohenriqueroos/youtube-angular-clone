927ecd7f0cfeb22fbf732a3e262127b3dae5f7a7
Henrique Roos <henriquemroos21@gmail.com>
2023-02-27 17:00:31 -0300

    feat: Correct Layout Shorts Component

--- src/app/components/shorts/shorts.component.ts
+++ src/app/components/shorts/shorts.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { ToggleMenuService } from 'src/app/shared/toggle-menu.service';
+
 @Component({
   selector: 'app-shorts',
   templateUrl: './shorts.component.html',
   styleUrls: ['./shorts.component.scss']
 })
 export class ShortsComponent implements OnInit {
+  menuStatus!: Observable<boolean>;
 
-  constructor() { }
+  constructor(private toggleMenuService: ToggleMenuService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.menuStatus = this.toggleMenuService.menuSelected$
   }
 
 }
